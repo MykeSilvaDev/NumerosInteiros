35b590957fd86464a05992c42084b15222d6f6ab
Myke Silva <119154150+Spike2247@users.noreply.github.com>
2024-01-14 20:17:33 -0300

    Adicionar arquivos de projeto.

---
+++ NumerosInteiros/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NumerosInteiros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // Nas expressoes da matemática, acontece primeiro a multiplicação e divisão
+            int d = 5;
+            int e = 4;
+            int f = 2;
+            int g = d + e * f;
+            Console.WriteLine(g);
+
+            // Para calcular primeiro a soma, devo utilizar o sinal de parênteses
+            int h = 5;
+            int i = 4;
+            int j = 2;
+            int k = (h + e) * j;
+            Console.WriteLine(k);
+
+            int l = 5;
+            int m = 4;
+            int n = 2;
+            int o = (l + m) - 6 * n + (12 * 4) / 3 + 12;
+            Console.WriteLine(k);
+
+        }
+    }
+}
